4b791a6256367f0a94319a0aaba56ffcf8300601
Julien Jet <jet.julien@gmail.com>
2016-11-23 16:38:36 -0200

    Add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews, only: [:new, :create]
   end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
